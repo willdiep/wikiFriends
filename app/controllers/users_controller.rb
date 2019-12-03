0fc991ece90ba31c76b08b02b215cab193d11a2d
dawncronin <github email address>
2019-12-03 14:53:02 -0800

    added user controller routes, and user views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,55 @@
 class UsersController < ApplicationController
+
+    def home
+    end
+
+    def index
+        @users = User.all
+    end
+
+    def show
+        @user = User.find(params[:id])
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.create(user_params)
+        if @user
+            session[:user_id] = @user.id
+            redirect_to @user
+        else
+            flash[:errors] = @user.errors.full_messages
+            redirect_to 'new'
+        end
+    end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            redirect_to @user
+        else
+            flash[:errors] = @user.errors.full_messages
+            redirect_to 'new'
+        end 
+    end
+
+    def destroy
+        User.find(params[:id]).destroy
+        session[:user_id] = nil
+        redirect_to "/"
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :first_name, :last_name, :password, :password_confirmation)
+    end
+
 end
