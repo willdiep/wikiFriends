99325449c1d102f7b36641ac1f77ed0e54cf95cc
Will Diep <widiep@gmail.com>
2019-12-03 14:51:38 -0800

    Create session controller and session 'new' view template

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+    def new
+    end
+  
+    def create
+      # byebug
+      @user = User.find_by(username: params[:username])
+      if @user && @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        # byebug
+        redirect_to @user
+      else  
+        flash[:error] = "Invalid username or password."
+        redirect_to login_path
+      end
+    end
+  
+    def destroy
+      session[:user_id] = nil
+      redirect_to login_path
+    end
+  end
