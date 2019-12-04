a4a7090f1348ea321d559b7f79cb3a70f515297e
dawncronin <github email address>
2019-12-04 14:17:12 -0800

    edited migrations, and added new controller files

--- db/migrate/20191203011928_create_saved_articles.rb
+++ db/migrate/20191203011928_create_saved_articles.rb
@@ -1,8 +1,8 @@
 class CreateSavedArticles < ActiveRecord::Migration[6.0]
   def change
     create_table :saved_articles do |t|
-      t.references :user_id, null: false, foreign_key: true
-      t.references :article_id, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.references :article, null: false, foreign_key: true
       t.string :note
 
       t.timestamps
