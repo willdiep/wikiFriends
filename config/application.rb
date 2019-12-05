8d0269abd5f9111a7f8f90eec68182f15b21ecea
dawncronin <github email address>
2019-12-05 14:44:07 -0800

    merged with master

--- config/application.rb
+++ config/application.rb
@@ -2,12 +2,6 @@ require_relative 'boot'
 
 require 'rails/all'
 
-<<<<<<< HEAD
-
-
-
-=======
->>>>>>> format
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
