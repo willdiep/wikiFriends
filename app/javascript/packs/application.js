30b940dbfa216977975b755b814c2fad07c8550b
Will Diep <widiep@gmail.com>
2019-12-09 12:05:39 -0800

    Troubleshooting theme switch - jQuery is not loading

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,8 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+
+
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
@@ -10,8 +12,13 @@ require("channels")
 
 require("jquery");
 require("bootstrap");
+require("packs/theme-switch");
 
 
+window.jQuery = $;
+window.$ = $;
+
+// window.$ = window.jQuery = require('packs/jquery')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -24,7 +31,6 @@ import "../src/main.scss";
 import "../src/users.scss";
 import "../src/util.scss";
 
-
-
-
 import "../src/custom.scss";
+// import "../src/theme-switch.scss";
+
